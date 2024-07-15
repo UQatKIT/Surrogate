912102976b50fbaf99efb8c400e66dbd50b28104
Zihua Niu <zniu2025@login2.frontera.tacc.utexas.edu>
2024-07-15 09:18:57 -0500

    merge local change for seissol 1D setup with main branch

--- seissol/zihua/settings_control.py
+++ seissol/zihua/settings_control.py
@@ -8,15 +8,15 @@ import src.surrogate.utilities as utils
 
 # ==================================================================================================
 simulation_model_settings = utils.SimulationModelSettings(
-    url="http://localhost:4242",
-    name="forward",
+    url="http://localhost:4343",
+    name="QueuingModel",
 )
 
 surrogate_model_type = surrogate_model.SKLearnGPSurrogateModel
 
 surrogate_model_settings = surrogate_model.SKLearnGPSettings(
     scaling_kernel=ConstantKernel(constant_value=0.5, constant_value_bounds=(1e-5, 1e5)),
-    correlation_kernel=RBF(length_scale=1e6, length_scale_bounds=(1e5, 1e8)),
+    correlation_kernel=RBF(length_scale=0.1, length_scale_bounds=(1e-2, 1e1)),
     data_noise=1e-6,
     num_optimizer_restarts=3,
     minimum_num_training_points=3,
@@ -28,8 +28,8 @@ surrogate_model_settings = surrogate_model.SKLearnGPSettings(
     log_mean_underflow_value=-1000,
     mean_underflow_value=1e-6,
     init_seed=0,
-    checkpoint_load_file="results_seissol_zihua_1D/surrogate_checkpoint_pretraining.pkl",
-    checkpoint_save_path=Path("results_seissol_zihua_1D"),
+    checkpoint_load_file="results_T14_seissol_zihua/surrogate_checkpoint_0.pkl",
+    checkpoint_save_path=Path("results_T22_seissol_zihua"),
 )
 
 # --------------------------------------------------------------------------------------------------
@@ -40,12 +40,12 @@ surrogate_control_settings = surrogate_control.ControlSettings(
     variance_threshold=1e-3,
     update_interval_rule=lambda num_updates: num_updates + 1,
     checkpoint_load_file=None,
-    checkpoint_save_path=Path("results_seissol_zihua_1D"),
+    checkpoint_save_path=Path("results_T22_seissol_zihua"),
     overwrite_checkpoint=False,
 )
 
 control_logger_settings = utils.LoggerSettings(
     do_printing=True,
-    logfile_path=Path("results_seissol_zihua_1D/online.log"),
+    logfile_path=Path("results_T22_seissol_zihua/online.log"),
     write_mode="w",
 )
--- seissol/zihua/settings_pretraining.py
+++ seissol/zihua/settings_pretraining.py
@@ -8,15 +8,15 @@ import src.surrogate.utilities as utils
 
 # ==================================================================================================
 simulation_model_settings = utils.SimulationModelSettings(
-    url="http://localhost:4242",
-    name="forward",
+    url="http://localhost:4343",
+    name="QueuingModel",
 )
 
 surrogate_model_type = surrogate_model.SKLearnGPSurrogateModel
 
 surrogate_model_settings = surrogate_model.SKLearnGPSettings(
     scaling_kernel=ConstantKernel(constant_value=0.5, constant_value_bounds=(1e-5, 1e5)),
-    correlation_kernel=RBF(length_scale=1e6, length_scale_bounds=(1e5, 1e8)),
+    correlation_kernel=RBF(length_scale=0.1, length_scale_bounds=(1e-2, 1e1)),
     data_noise=1e-6,
     num_optimizer_restarts=3,
     minimum_num_training_points=3,
@@ -29,21 +29,21 @@ surrogate_model_settings = surrogate_model.SKLearnGPSettings(
     mean_underflow_value=1e-6,
     init_seed=0,
     checkpoint_load_file=None,
-    checkpoint_save_path=Path("results_seissol_zihua_1D"),
+    checkpoint_save_path=Path("results_sigyS_seissol_zihua"),
 )
 
 # --------------------------------------------------------------------------------------------------
 pretraining_settings = offline_training.OfflineTrainingSettings(
     num_offline_training_points=5,
     num_threads=5,
-    offline_model_config={"order": 4},
-    lhs_bounds=[[0, 1e7]],
+    offline_model_config={"meshFile": "Ridgecrest_NewModel1_f200_topo1000_noRef_xml_UBC","order": 3},
+    lhs_bounds=[[0.5, 3.0]],
     lhs_seed=0,
     checkpoint_save_name="pretraining",
 )
 
 pretraining_logger_settings = utils.LoggerSettings(
     do_printing=True,
-    logfile_path=Path("results_seissol_zihua_1D/pretraining.log"),
+    logfile_path=Path("results_sigyS_seissol_zihua/pretraining.log"),
     write_mode="w",
 )
